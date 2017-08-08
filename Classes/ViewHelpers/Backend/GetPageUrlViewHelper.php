68bdb0b5e42a61ee15ee772e6dc3e7743e0ee111
Steffen Thierock <steffen@thierock.de>
2017-08-08 15:53:36 +0200

    ext vhlib appears

---
+++ Classes/ViewHelpers/Backend/GetPageUrlViewHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TgM\Vhlib\ViewHelpers\Backend;
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class GetPageUrlViewHelper extends AbstractViewHelper {
+
+	/**
+	 * Builds an url to view a page in the backend.
+	 *
+	 * @param int   $uid    The uid of the page to open.
+	 *
+	 * @return string The final url
+	 */
+	public function render($uid) {
+        // TODO: check userrights, check compatibility with workspaces, check if uid exists
+
+		$url = '?M=' . $_GET['M'] . '&moduleToken=' . $_GET['moduleToken'] . '&id=' . $uid;
+
+		return $url;
+	}
+}
---
+++ Classes/ViewHelpers/GetRecordViewHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TgM\Vhlib\ViewHelpers;
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+
+class GetRecordViewHelper extends AbstractViewHelper {
+
+	/**
+	 * TODO: languages, workspaces and userrights
+	 * @param string  $table
+	 * @param integer $uid
+	 *
+	 * @return array|bool
+	 */
+	public function render($table, $uid) {
+		$result = BackendUtility::getRecord($table, $uid);
+		return (empty($result) === false) ? $result : false;
+	}
+}
